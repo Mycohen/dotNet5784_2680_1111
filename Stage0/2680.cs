c3757acbf9989c7bdf8e0a216a126d62b7b82744
Moshe Jacob Cohen <moshejco@g.jct.ac.il>
2023-12-27 12:48:32 +0200

    Initial Code + Welcome functions

--- Stage0/2680.cs
+++ Stage0/2680.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace Targil_0 // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Welcome2680();
+            Console.ReadKey();
+            Welcome1111();
+
+        }
+        static partial void welcome1111();
+        private static void Welcome2680()
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console aplication");
         }
     }
 }
