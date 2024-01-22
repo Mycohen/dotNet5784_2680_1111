91167f28949e48d9eeed3620f593ebae5c9baecd
Moshe Jacob Cohen <moshejco@g.jct.ac.il>
2024-01-22 20:22:08 +0200

    With xml config and tool file

---
+++ DalXml/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+
+internal static class Config
+{
+    static string s_data_config_xml = "data-config";
+    internal static int NextTaskId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); }
+    internal static int NextDepId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextDepId"); }
+
+}
+
+
