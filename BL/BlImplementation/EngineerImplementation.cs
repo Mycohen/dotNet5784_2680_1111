8ec398727ea80e320730d500ea4cdd93b7f16e83
Moshe Jacob Cohen <moshejco@g.jct.ac.il>
2024-02-12 15:01:41 +0200

    For Shmuel

---
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlImplementation;
+using BlApi;
+using BO;
+
+internal class EngineerImplementation : IEngineer
+{
+    private DalApi.IDal _dal = Factory.Get;
+
+    public int Create(Engineer item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Engineer? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Engineer? Read(Func<Task, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TaskInEngineer> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Engineer item)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BL/BlImplementation/MilestoneImplementation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlImplementation;
+    using BlApi;
+using BO;
+
+internal class MilestoneImplementation : IMilestone
+{
+    public int Create(Milestone item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Milestone? Read(Func<Milestone, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Milestone? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Milestone> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Milestone?> ReadAll(Func<Milestone, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Milestone item)
+    {
+        throw new NotImplementedException();
+    }
+}
+
