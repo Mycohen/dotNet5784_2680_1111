a3f7ec2beb86f85ba9c7d3aa775dc486d5cacf1a
Shmuel Kaplan <shmuelkaplan02@gmail.com>
2024-03-03 14:33:35 +0200

    Commit with the function of the reqursive udate of the scheduale date

--- BL/BO/Tools.cs
+++ BL/BO/Tools.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DalApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace BO;
 
 internal static class  Tools
 {
-  
+   
+   
 }
